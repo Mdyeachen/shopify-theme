d748078e6410b730b8067b29880bd9745d25b849
Yeachen29 <yeachenabir29@gmail.com>
2025-06-01 02:18:36 +0600

    add tailwindcss postcss autoprefix

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./*.liquid",
+    "./layout/**/*.liquid",
+    "./templates/**/*.liquid",
+    "./sections/**/*.liquid",
+    "./snippets/**/*.liquid",
+    "./assets/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
